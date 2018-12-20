dba6232359f7eed6ed7b41cbfc86cbd45689b990
Ben Franzi <benfranzi@outlook.com>
2018-12-21 09:52:47 +1100

    Initial commit to master

---
+++ packages/server/src/app.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const path = require('path')
+const app = express()
+
+app.use(express.static(path.join(__dirname,'../../frontend/dist/')))
+
+app.get('/api/dummydata', (req, res) => {
+    res.json({
+        userId: 1,
+        id: 1,
+        title: "hello world",
+        completed: false
+    })
+})
+
+const port = process.env.PORT || 8080
+app.listen(port)
+console.log(`running on :${port}`)
